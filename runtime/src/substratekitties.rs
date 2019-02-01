8370c9f78bfffe0093c1dd471c8ba16c06a85f3c
iveltondequeirozm <iveltondequeiroz@gmail.com>
2019-02-01 16:15:21 -0400

    Substratekitties module added

---
+++ runtime/src/substratekitties.rs
@@ -0,0 +1,13 @@
+pub trait Trait: system::Trait {}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as KittyStorage {
+        // Declare storage and getter functions here
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // Declare public functions here
+    }
+}
