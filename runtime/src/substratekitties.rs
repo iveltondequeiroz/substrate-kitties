2e4c472627b1f6fce8d04561a308fe403f53fe75
iveltondequeirozm <iveltondequeiroz@gmail.com>
2019-02-03 11:50:43 -0400

    Kitty Structure added

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,21 +1,37 @@
+use runtime_primitives::traits::{As, Hash};
 use srml_support::{dispatch::Result, StorageMap};
 use system::ensure_signed;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
 
 pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        Value: map T::AccountId => u64;
+        OwnedKitty: map T::AccountId => Kitty<T::Hash, T::Balance>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-        fn set_value(origin, value: u64) -> Result {
+        fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            <Value<T>>::insert(sender, value);
+            let new_kitty = Kitty {
+                id: <T as system::Trait>::Hashing::hash_of(&0),
+                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                price: <T::Balance as As<u64>>::sa(0),
+                gen: 0,
+            };
+
+            <OwnedKitty<T>>::insert(&sender, new_kitty);
 
             Ok(())
         }
