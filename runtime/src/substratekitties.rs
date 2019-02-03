0b7ab8cc5172eedfff7e685b6e9efb95352e6347
iveltondequeirozm <iveltondequeiroz@gmail.com>
2019-02-03 10:29:41 -0400

    store value to map

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,13 +1,23 @@
-pub trait Trait: system::Trait {}
+use srml_support::{dispatch::Result, StorageMap};
+use system::ensure_signed;
+
+pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        // Declare storage and getter functions here
+        Value: map T::AccountId => u64;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Declare public functions here
+
+        fn set_value(origin, value: u64) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            <Value<T>>::insert(sender, value);
+
+            Ok(())
+        }
     }
 }
